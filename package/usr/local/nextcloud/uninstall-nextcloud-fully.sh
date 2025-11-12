fc21082f72755dcf548449ecbc42d8b228c3941e
kr1sal <kirillivanov523@gmail.com>
2025-11-12 12:54:29 +0300

    fix: opsu pupsu delete cert manager

--- package/usr/local/nextcloud/uninstall-nextcloud-fully.sh
+++ package/usr/local/nextcloud/uninstall-nextcloud-fully.sh
@@ -7,10 +7,6 @@ if helm --kubeconfig $K3S_CONFIG_FILE list -n nextcloud | grep -q nextcloud-serv
   helm --kubeconfig $K3S_CONFIG_FILE uninstall nextcloud-server -n nextcloud
 fi
 
-if helm --kubeconfig $K3S_CONFIG_FILE list -n cert-manager | grep -q cert-manager; then
-  helm --kubeconfig $K3S_CONFIG_FILE uninstall cert-manager -n cert-manager
-fi
-
 if helm --kubeconfig $K3S_CONFIG_FILE list -n nextcloud | grep -q postgresql; then
   helm --kubeconfig $K3S_CONFIG_FILE uninstall postgresql -n nextcloud
 fi
@@ -19,17 +15,10 @@ if helm --kubeconfig $K3S_CONFIG_FILE list -n nextcloud | grep -q nfs-provisione
   helm --kubeconfig $K3S_CONFIG_FILE uninstall nfs-provisioner -n nextcloud
 fi
 
-if systemctl status nextcloud-port-forward.service | grep -q " active "; then
-  sudo systemctl disable --now nextcloud-port-forward.service
-fi
-
 if sudo k3s kubectl get namespace nextcloud >/dev/null 2>&1; then
   sudo k3s kubectl delete namespace nextcloud
 fi
 
-if sudo k3s kubectl get namespace cert-manager >/dev/null 2>&1; then
-  sudo k3s kubectl delete namespace cert-manager
-fi
 
 echo "You need manually delete PV from cluster"
 echo "  sudo k3s kubectl get pv"
--- package/usr/local/nextcloud/uninstall-nextcloud.sh
+++ package/usr/local/nextcloud/uninstall-nextcloud.sh
@@ -7,18 +7,10 @@ if helm --kubeconfig $K3S_CONFIG_FILE list -n nextcloud | grep -q nextcloud-serv
   helm --kubeconfig $K3S_CONFIG_FILE uninstall nextcloud-server -n nextcloud
 fi
 
-if helm --kubeconfig $K3S_CONFIG_FILE list -n cert-manager | grep -q cert-manager; then
-  helm --kubeconfig $K3S_CONFIG_FILE uninstall cert-manager -n cert-manager
-fi
-
 if helm --kubeconfig $K3S_CONFIG_FILE list -n nextcloud | grep -q postgresql; then
   helm --kubeconfig $K3S_CONFIG_FILE uninstall postgresql -n nextcloud
 fi
 
 if helm --kubeconfig $K3S_CONFIG_FILE list -n nextcloud | grep -q nfs-provisioner; then
   helm --kubeconfig $K3S_CONFIG_FILE uninstall nfs-provisioner -n nextcloud
-fi
-
-if systemctl status nextcloud-port-forward.service | grep -q " active "; then
-  sudo systemctl disable --now nextcloud-port-forward.service
 fi
